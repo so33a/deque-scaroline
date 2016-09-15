d19a0a55f09bfd17ff39349471b8963e10e84729
scaroline <ssouza@alunos.utfpr.edu.br>
2016-09-14 21:05:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "fila.h"
-
+#include "fila.c"
 int main () {
   FILA alunos = novaFila();
   inserir(alunos, 9);
@@ -14,11 +14,16 @@ int main () {
     printf ("removido: %d \n", remover(alunos));
 
 
-  inserir(alunos, 10);
-  inserir(alunos, 32);
+	inserir(alunos, 10);
+	inserir(alunos, 32);
+	inseriresq(alunos, 5);
+	
+	imprimirFila(alunos);
+	
+	removerdireita(alunos); 
+	imprimirFila(alunos);
+	
+	destroiFila(alunos);
   
-  imprimirFila(alunos);
-
-  destroiFila(alunos);
   return 0;
 }
